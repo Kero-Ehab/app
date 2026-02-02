d6d59d94d50f5c46e1437ce322a5f0867f87d4c5
kero <kerolosehabgaber@gmail.com>
2026-02-02 17:17:54 +0200

    Enhance User schema and service with additional fields and validation

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -4,9 +4,76 @@ import { HydratedDocument } from 'mongoose';
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({timestamps: true})
-export class User {
-    @Prop()
+export class User {  
+    @Prop({
+        required:true,
+        type:String,
+        min:[3,'Name must be at least 3 characters'],
+        max:[30,'Name must be at most 3 characters'],
+    })
     name: string;
+
+    @Prop({
+        required:true,
+        type:String,
+        unique:true,
+    })
+    email: string;
+
+    @Prop({
+        required:true,
+        type:String,
+        min:[3,'Password must be at least 3 characters'],
+        max:[30,'Password must be at most 3 characters'],
+    })
+    password: string;
+
+    @Prop({
+        type:String,
+        required:true,
+        enum:['user', 'admin'],
+    })
+    role: string;
+
+    @Prop({
+        type:String,
+        required: false 
+    })
+    avatar?: string;
+
+    @Prop({
+        type:Number,
+    })
+    age: string;
+
+    @Prop({
+        type:Number,
+    })
+    phoneNumber: string;
+
+    @Prop({
+        type:String,
+    })
+    address: string;
+
+    @Prop({
+        type:String,
+        enum:[false, true]
+    })
+    active: string;
+
+    @Prop({
+        type:String,
+    })
+    verificationCode: string;
+
+    @Prop({
+        type:String,
+        enum:['male', 'female']
+    })
+    gender: string;
+
+
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
